369728211a3ec281a46862b0856a9602792c9c7a
Vibhore Jain <vibhore@pcplusa.com>
2025-11-24 21:40:01 +0530

    rwewrwe

--- frontend/src/components/Vibhore.tsx
+++ frontend/src/components/Vibhore.tsx
@@ -1,5 +1,5 @@
 const Vibhore = () => {
-  return <div className="p-4 bg-gray-100">Vibhore component3</div>;
+  return <div className="p-4 bg-gray-100">Vibhore component3777</div>;
 };
 
 export default Vibhore;
