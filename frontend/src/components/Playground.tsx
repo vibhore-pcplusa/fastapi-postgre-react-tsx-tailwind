775ba004cfa2d6db0b283878fd6def5dadf7d101
Vibhore Jain <vibhore@pcplusa.com>
2025-12-04 21:37:24 +0530

    playing with react done

--- frontend/src/components/Playground.tsx
+++ frontend/src/components/Playground.tsx
@@ -1,6 +1,8 @@
 import {useState,useEffect,useRef} from 'react';
 const Playground = () => {
   const[count,setCount]=useState(0);
+  const[mycolor,setMycolor]=useState("");
+  //setMycolor
   const myref=useRef<HTMLInputElement>(null);
   const handletextbox=async() =>{
     if (myref.current) {
@@ -10,18 +12,26 @@ const Playground = () => {
   const handletextboxadd=async() =>{
     setCount(count+1);
   };
+
   const changetextboxcolor = async() =>{
     if(myref.current)
     { 
-    myref.current.style.backgroundColor="blue";
+      myref.current.style.backgroundColor=mycolor;    //"Orange";
     }
   }
+
   const removetextboxcolor = async() =>{
     if(myref.current)
     { 
     myref.current.style.backgroundColor="white";
     }
   }
+
+  const handlechangedrop = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log('Selected color:', e.target.value);
+    setMycolor(e.target.value);
+  };
+
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold mb-4">Playground</h1>
@@ -31,7 +41,12 @@ const Playground = () => {
       <input type="text" ref={myref} value={count}/>
       <input type="button" onClick={handletextbox} value="do focus" style={{border: "2px solid red", borderRadius: "2px"}}/>
       <input type="button" onClick={handletextboxadd} value="add value one" style={{border: "2px solid red", borderRadius: "2px"}}/>
-      <input type="button" onClick={changetextboxcolor} value="cange textbox color" style={{border: "5px solid blue", borderRadius: "5px"}}/>
+      <select onChange={handlechangedrop}>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </select>
+      <input type="button" onClick={changetextboxcolor} value="change textbox color" style={{border: "5px solid blue", borderRadius: "5px"}}/>
       <input type="button" onClick={removetextboxcolor} value="remove textbox color" style={{border: "5px solid green", borderRadius: "5px"}}/>
     
     </div>
